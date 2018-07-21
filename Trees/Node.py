462f08a1f4c8cf14a2459abe507ca56fe99f475d
Ian <ioloughlin.ct.oem@gmail.com>
2018-07-21 10:58:19 -0400

    Updated to show left or right node level

--- Trees/Node.py
+++ Trees/Node.py
@@ -6,13 +6,20 @@ class Node:
         self.left = None
         self.right = None
         self.val = val
+        self.indentLevel = ""
 
     def print_val(self):
-        print(self.val)
         if self.left is not None:
+            print(self.indentLevel + " " + self.val)
+            self.left.indentLevel = self.indentLevel + "L"
             self.left.print_val()
         if self.right is not None:
+            print(self.indentLevel + " " + self.val)
+            self.right.indentLevel = self.indentLevel + "R"
             self.right.print_val()
+        if self.left is None and self.right is None:
+            print(self.indentLevel + " leaf " + self.val)
+
 
 
 
--- Trees/Tree.py
+++ Trees/Tree.py
@@ -9,11 +9,17 @@ leftNode2 = Node("leftNode2")
 rightNode = Node("rightNode")
 rightNode2 = Node("rightNode2")
 
+nodel3 = Node("node Level 3")
+
 root.left = leftNode
 root.right = rightNode
 
 leftNode.left = leftNode2
 leftNode.right = rightNode2
+
+rightNode2.left = nodel3
+
+
 
 root.print_val()
 
